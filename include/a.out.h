4e9a31326b18263aede8a3a3c07236ea79a0c06b
Charles Forsyth <charles.forsyth@gmail.com>
2014-06-22 21:52:24 +0100

    mkext is not needed for compiler suite

--- include/a.out.h
+++ include/a.out.h
@@ -11,7 +11,6 @@ struct	Exec
 	long	pcsz;		/* size of pc/line number table */
 };
 
-
 #define HDR_MAGIC	0x00008000		/* header expansion */
 
 #define	_MAGIC(f, b)	((f)|((((4*(b))+0)*(b))+7))
@@ -26,20 +25,23 @@ struct	Exec
 #define	E_MAGIC		_MAGIC(0, 20)		/* arm */
 #define	Q_MAGIC		_MAGIC(0, 21)		/* powerpc */
 #define	N_MAGIC		_MAGIC(0, 22)		/* mips 4000 LE */
-#define	L_MAGIC		_MAGIC(0, 23)		/* dec alpha */
+#define	L_MAGIC		_MAGIC(0, 23)		/* dec alpha (retired) */
 #define	P_MAGIC		_MAGIC(0, 24)		/* mips 3000 LE */
-#define	U_MAGIC		_MAGIC(0, 25)		/* sparc64 */
+#define	U_MAGIC		_MAGIC(0, 25)		/* sparc64 (retired) */
 #define	S_MAGIC		_MAGIC(HDR_MAGIC, 26)	/* amd64 */
+#define	T_MAGIC		_MAGIC(HDR_MAGIC, 27)	/* powerpc64 */
+#define	R_MAGIC		_MAGIC(HDR_MAGIC, 28)	/* arm64 */
 
 #define	MIN_MAGIC	8
-#define	MAX_MAGIC	26			/* <= 90 */
+#define	MAX_MAGIC	28			/* <= 90 */
 
-#define	DYN_MAGIC	0x80000000	/* or'd in for dynamically loaded modules */
+#define	DYN_MAGIC	0x80000000		/* dlm */
 
 typedef	struct	Sym	Sym;
 struct	Sym
 {
-	long	value;
+	vlong	value;
+	uint	sig;
 	char	type;
 	char	*name;
 };
